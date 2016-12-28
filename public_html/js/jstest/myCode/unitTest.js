c2e36f9a748ea81a8c627274ff2daec0c2ed8e1f
yichon <yi.chong.oha@gmail.com>
2016-12-28 17:03:38 +0800

    201612281703

--- public_html/js/jstest/myCode/unitTest.js
+++ public_html/js/jstest/myCode/unitTest.js
@@ -11,29 +11,40 @@ function test1(obj, func, input, expected, counter, cpf) {
                 e.message = "'obj' is null";
             if (typeof func !== 'string')
                 e.message = "'func' - not a string";
-            if (typeof obj[func] !== 'function')
+            if (obj[func] && typeof obj[func] !== 'function')
                 e.message = "'obj[func]' - undefined or illegal type";
+            if (!obj[func] && input)
+                e.message = "method - not exist";
             if (!(input instanceof Array || input === null || input === undefined))
                 e.message = "'input' - illegal type";
+            if (!expected || typeof expected !== 'object' ||
+                    !("value" in expected || expected.type || expected.error))
+                e.message = "'expected' - illegal type";
             if ((typeof counter !== 'number') || isNaN(counter))
                 e.message = "'counter' - not a number";
             if (e.message !== "Default")
                 throw e;
         };
         checkParameter(obj, func, input, expected, counter, cpf);
-
         let compare = cpf ? cpf : function (a, b) { // compare primitive values? or Objects?
             return a === b;
         };
-        let output, result;
+        let output, expect, result;
         if (input === undefined) // property
             output = obj[func];
         else // method
             output = obj[func].apply(obj, input);
-        result = compare(output, expected) ? 'Success' : '*Failed';
+        if ("value" in expected)
+            expect = expected.value;
+        else if (expected.type){
+            output = typeof output;
+            expect = expected.type;
+        }else{
+            
+        }
+        result = compare(output, expect) ? 'Success' : '*Failed';
         console.log((ct++) + ".", "[" + result + "]:", "Output:",
-                output, ";", "Expected: " + expected);
-
+                output, ";", "Expected: " + expect);
     } catch (err) {
         if (expected.error && expected.error === err.message)
             console.log((ct++) + ".", "[Success]:", "Output:",
@@ -93,7 +104,8 @@ function testMI(obj, func, inputlist, expected, counter, cpf) {
             console.log((ct++) + ".", "[" + result + "]:", "Output:", output, ";", "Expected: " + expected);
         }
     } catch (err) {
-        if (expected.error && expected.error === err.message)
+        if (expected && typeof expected === 'object'
+                && expected.error && expected.error === err.message)
             console.log((ct++) + ".", "[Success]:", "Output:",
                     err.message, ";", "Expected: " + expected.error);
         else
